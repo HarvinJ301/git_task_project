171fd54f93434b12bd03e0effb7231fc6c196bac
harvin.johal <harvin.johal@fdmgroup.com>
2024-03-13 00:52:21 +0000

    added comments to the file and the add function

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,20 @@
+#input to get user age
 userAge = int(input("Please input your age: "))
+#input to get user's name
 userName = str(input("Please enter your name: "))
+#input to get user's postcode
 userPostCode = str(input("Please enter your home postcode: "))
 
+#print out the user's inputs
 print(userName)
 print(userAge)
 print(userPostCode)
+
+#function to add two numbers
+def addNums(x, y):
+    #the addition and output
+    addingTogether = x + y
+    print(f"The answer to {x} + {y} is: {addingTogether}")
+
+#calling the function and getting two numbers from the user
+addNums(int(input("Please enter the first number for the addition: ")), int(input("Please enter the second number for the addition: ")))
